448b2e73c85d4850895045183867cff98c732112
leecjson <leecjson@gmail.com>
2018-12-29 14:48:46 +0800

    Remove '-Recurse' parameter

--- primesum/index.ps1
+++ primesum/index.ps1
@@ -1,4 +1,4 @@
-ForEach ($_ in Get-ChildItem -Path ".\" -Recurse -ErrorAction SilentlyContinue -Filter *.ps1) {
+ForEach ($_ in Get-ChildItem -Path ".\" -ErrorAction SilentlyContinue -Filter *.ps1) {
   if($_ -is [System.IO.FileInfo] -and ($_.name -ne "index.ps1")) 
   {
     powershell (".\" + $_.name);
