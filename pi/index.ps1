7507405e114f30a511dd27e3bf3e8e42bb42e9f7
anydream <anydreamplay@hotmail.com>
2018-12-30 10:46:06 +0800

    Print name for testing item

--- pi/index.ps1
+++ pi/index.ps1
@@ -1,6 +1,7 @@
 ForEach ($_ in Get-ChildItem -Path ".\" -ErrorAction SilentlyContinue -Filter *.ps1) {
   if($_ -is [System.IO.FileInfo] -and ($_.name -ne "index.ps1")) 
   {
+    Write-Host ("[" + $_.BaseName + "]")
     powershell (".\" + $_.name);
   }
 }
